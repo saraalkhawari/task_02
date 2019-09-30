9f6a56c61c6437f9bbf85e38d1347bd727bf69f7
Sara Alkhawari <sara.a.alkhawari@gmail.com>
2019-09-30 19:00:20 +0300

    commit task 2

--- restaurants/views.py
+++ restaurants/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def page1 (request):
+
+	context = {
+
+		'msg': 'Hello World !'
+	}
+
+	return render(request,'page1.html',context)
